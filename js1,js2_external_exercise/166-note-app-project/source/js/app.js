a96f90ac723447f7b1166c0002fd955a0884dd91
Mahdi-Barahimi <mahdi.barahimi.dev@gmail.com>
2023-06-04 22:58:54 +0430

    refactor click on the note to remove it with set event on parent of note(Bubbling)

--- js1,js2_external_exercise/166-note-app-project/source/js/app.js
+++ js1,js2_external_exercise/166-note-app-project/source/js/app.js
@@ -4,7 +4,11 @@ const colorBox = $.querySelectorAll('.color-box');
 const saveBtn = $.getElementById('btn-save');
 const deleteBtn = $.getElementById('btn-delete');
 const listed = $.getElementById('listed');
-let cardNote = $.querySelectorAll('.card');
+
+// // commented for refactor at line:33 --------------//
+// let cardNote = $.querySelectorAll('.card');       //
+// // ----------------------------------------------//
+
 colorBox.forEach(function (color) {
     color.addEventListener('click', function () {
         inputNote.style.backgroundColor = color.style.backgroundColor;
@@ -25,13 +29,27 @@ function addNote() {
         newNoteDiv.appendChild(newNoteP);
         listed.appendChild(newNoteDiv);
         clearInput();
-        cardNote = $.querySelectorAll('.card');
-        newNoteDiv.addEventListener('click', removeNote)
+        // // commented for refactor at line:33 -------------------//
+        // cardNote = $.querySelectorAll('.card');                //
+        // newNoteDiv.addEventListener('click', removeNote)      //
+        // // --------------------------------------------------//
     }
 }
-function removeNote(event) {
-    event.target.parentElement.remove();
-}
+
+// refactor click on the note to remove it with set one event on parent of note (Bubbling) //
+listed.addEventListener('click', function (event) {                                       //
+    if (event.target.tagName === 'P') {                                                  //
+        event.target.parentElement.remove();                                            //
+    }                                                                                  //
+})                                                                                    //
+// ----------------------------------------------------------------------------------//
+
+// // commented for refactor at line:33 --------------//
+// function removeNote(event) {                      //
+//     event.target.parentElement.remove();         //
+// }                                               //
+// // --------------------------------------------//
+
 saveBtn.addEventListener('click', addNote);
 deleteBtn.addEventListener('click', clearInput)
 $.body.addEventListener('keydown', function (event) {
