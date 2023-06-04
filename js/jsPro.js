98e8c547dad4e18f36d2465681e156b3f7511228
Mahdi-Barahimi <mahdi.barahimi.dev@gmail.com>
2023-06-04 22:25:21 +0430

    up to ep 263: event bubbling and event capturing

--- js/jsPro.js
+++ js/jsPro.js
@@ -10,11 +10,30 @@ eventOnceOptionBtn.addEventListener('click', function () {
     console.log('click');
 }, {once:true})
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// event bubbling (from child to parent element)
+// event Bubbling and event Capturing
+
+// event Bubbling (from child to parent element)
 const eventBubblingDiv = $.querySelectorAll('.eventBubbling');
-eventBubblingDiv.forEach(function (div, index) {
-    div.addEventListener('click', function (event) {
-        console.log("div " + (index + 1));
-        event.target.style.backgroundColor = 'skyBlue'
+eventBubblingDiv.forEach(function (divB, index) {
+    divB.addEventListener('click', function () {
+        console.log('div ' + (index + 1));
+
+    })
+})
+
+// event Capturing (from parent to child element)
+const eventCapturingDiv = $.querySelectorAll('.eventCapturing');
+eventCapturingDiv.forEach(function (divC, index) {
+    divC.addEventListener('click', function () {
+        console.log('div ' + (index + 1));
+    }, {capture:true})
+})
+
+// stop event Bubbling with event.stopPropagation()
+const stopEventBubbling = $.querySelectorAll('.stopEventBubbling');
+stopEventBubbling.forEach(function (divS, index) {
+    divS.addEventListener('click', function (event) {
+        event.stopPropagation()
+        console.log('div ' + (index + 1));
     })
 })
